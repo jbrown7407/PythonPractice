a67e77a9a53f2998894b82f8ee0f51e793cfa7ff
jbrown7407 <jbrown7407@gmail.com>
2021-02-08 12:14:45 -0500

    functiondec

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 print("hello world")
 # comment out using pound sign
 
+#variable declaration
 a = 33
 b = 200
 if b > a:
     print("b is greater than a")
 
+#for loop
 fruits = ["apple", "banana", "pear"]
 for x in fruits:
     print(x)
+
+ def my_function():
+     print("Hello from function 1")   
